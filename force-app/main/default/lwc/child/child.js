78775e885bf08fc2163f34916411a258f719d06e
narmatha-jehanathan <narmijehan@gmail.com>
2022-12-09 13:24:53 +0530

    Changes

--- force-app/main/default/lwc/child/child.js
+++ force-app/main/default/lwc/child/child.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class Child extends LightningElement {}
+export default class Child extends LightningElement {
+    text = 'Hello World!!';
+
+    handleClick(){
+        this.text = 'Welcome..'
+    }
+}
